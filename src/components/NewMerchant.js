2249249ff6e03b8aca41aab1fba31c060bc21e03
JuliaShevchuk <ishevchuk@rubiconproject.com>
2018-08-14 21:31:43 +0300

    add material components to new merchant view

--- src/components/NewMerchant.js
+++ src/components/NewMerchant.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Field } from 'redux-form';
+import CheckboxInput from './CheckboxInput';
+import TextInput from './TextInput';
+import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
+import { withStyles } from '@material-ui/core/styles';
 
-const NewMerchant = ({ handleSubmit }) => (
-  <div>
-    <Field name="firstname" component="input" placeholder="First Name" />
-    <Field name="lastname" component="input" placeholder="Last Name" />
-    <Field name="email" component="input" type="email" placeholder="Email" />
-    <Field
-      name="phone"
-      component="input"
-      type="tel"
-      placeholder="Phone Number"
-    />
-    <Field name="hasPremium" component="input" type="checkbox" />
-    <button onClick={handleSubmit}>Create new Merchant</button>
+const styles = theme => ({
+  container: {
+    padding: 2 * theme.spacing.unit
+  },
+  buttonIcon: {
+    marginRight: theme.spacing.unit
+  }
+});
+
+const NewMerchant = ({ handleSubmit, classes }) => (
+  <div className={classes.container}>
+    <Field name="firstname" component={TextInput} label="First Name" />
+    <Field name="lastname" component={TextInput} label="Last Name" />
+    <Field name="email" component={TextInput} type="email" label="Email" />
+    <Field name="phone" component={TextInput} type="tel" label="Phone Number" />
+    <Field name="hasPremium" component={CheckboxInput} label="Has premium" />
+    <Button variant="contained" color="primary" onClick={handleSubmit}>
+      <SaveIcon className={classes.buttonIcon} />
+      Create Merchant
+    </Button>
   </div>
 );
 
@@ -22,4 +34,4 @@ NewMerchant.propTypes = {
   handleSubmit: PropTypes.func.isRequired
 };
 
-export default NewMerchant;
+export default withStyles(styles)(NewMerchant);
